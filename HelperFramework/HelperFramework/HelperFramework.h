064b8fc17b5004486a6e5b1423d1495ab578632d
phani407 <phanibandaru23@gmail.com>
2020-05-15 11:25:04 +0530

    Finished our Framework

---
+++ HelperFramework/HelperFramework/HelperFramework.h
@@ -0,0 +1,19 @@
+//
+//  HelperFramework.h
+//  HelperFramework
+//
+//  Created by RamiReddy on 15/05/20.
+//  Copyright © 2020 RamiReddy. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for HelperFramework.
+FOUNDATION_EXPORT double HelperFrameworkVersionNumber;
+
+//! Project version string for HelperFramework.
+FOUNDATION_EXPORT const unsigned char HelperFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HelperFramework/PublicHeader.h>
+
+
---
+++ HelperFramework/Service.swift
@@ -0,0 +1,19 @@
+//
+//  Service.swift
+//  HelperFramework
+//
+//  Created by RamiReddy on 15/05/20.
+//  Copyright © 2020 RamiReddy. All rights reserved.
+//
+
+import Foundation
+
+
+public class Service {
+    
+    private init(){}
+    
+    public static func getMethod() -> String {
+        return "RamiReddy G"
+    }
+}
